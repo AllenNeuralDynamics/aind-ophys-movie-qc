192fd07c9dc88be62a23cb3b7e70ac4443f90799
Arielle Leon <ariellel@alleninstitute.org>
2023-10-11 20:53:53 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -585,7 +585,7 @@ if __name__ == "__main__":  # pragma: nocover
     dataset_name = "data"
 
     input_dir = Path(args.input_dir)
-    ouptut_dir = Path(args.output_dir)
+    output_dir = Path(args.output_dir)
     h5_file = [i for i in list(input_dir.glob("*/*")) if "registered.h5" in str(i)][0]
     experiment_id = h5_file.name.split("_")[0]
     output_dir = make_output_directory(output_dir, experiment_id)
