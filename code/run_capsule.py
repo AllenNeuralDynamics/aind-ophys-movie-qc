a0a4eb6400a37c572687d97d820085cd168cd2e2
Arielle Leon <ariellel@alleninstitute.org>
2023-10-11 19:40:51 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -496,6 +496,27 @@ def save_figure_to_storage(figure, storage_path, root_filename, image_name, dpi=
     figure.savefig(filepath, dpi=dpi)
     plt.close(figure)
 
+def make_output_directory(output_dir: str, experiment_id: str = None) -> str:
+    """Creates the output directory if it does not exist
+
+    Parameters
+    ----------
+    output_dir: str
+        output directory
+    experiment_id: str
+        experiment_id number
+
+    Returns
+    -------
+    output_dir: str
+        output directory
+    """
+    if experiment_id:
+        output_dir = os.path.join(output_dir, experiment_id)
+    else:
+        output_dir = os.path.join(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
+    return output_dir
 
 if __name__ == "__main__":  # pragma: nocover
     # Create an ArgumentParser object
