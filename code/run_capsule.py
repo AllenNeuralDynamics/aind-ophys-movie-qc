d87b93b9d84533cd8fad1a887d00b89d2f893e0f
Arielle Leon <ariellel@alleninstitute.org>
2024-10-22 13:07:08 -0700

    Clean up for transition to quality control.
    -Remove unneccessary imports.
    
    Remove write_output_metadata function.

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -8,62 +8,14 @@ import argparse
 import h5py
 import os
 import json
-from skimage import io, color, filters, measure
+from skimage import filters, measure
 import matplotlib.patches as patches
-from scipy import ndimage, signal
+from scipy import ndimage
 from pathlib import Path
 import glob
 import os 
-from aind_data_schema.core.processing import Processing, DataProcess, ProcessName, PipelineProcess
-from typing import Union
 from datetime import datetime as dt
 
-
-def write_output_metadata(
-    metadata: dict,
-    input_fp: Union[str, Path],
-    output_fp: Union[str, Path],
-    url: str,
-    start_date_time: dt,
-) -> None:
-    """Writes output metadata to plane processing.json
-
-    Parameters
-    ----------
-    metadata: dict
-        parameters from suite2p motion correction
-    input_fp: str
-        path to data input
-    output_fp: str
-        path to data output
-    url: str
-        url to code repository
-    """
-    original_proc_file = next(Path("../data").rglob("processing.json))
-    with open(original_proc_file / "processing.json", "r") as f:
-        proc_data = json.load(f)
-    prev_processing = Processing(**proc_data)
-    processing = Processing(
-        processing_pipeline=PipelineProcess(
-            processor_full_name="Multplane Ophys Processing Pipeline",
-            pipeline_url="https://codeocean.allenneuraldynamics.org/capsule/5472403/tree",
-            pipeline_version="0.1.0",
-            data_processes=[
-                DataProcess(
-                    name=ProcessName.VIDEO_PLANE_DECROSSTALK,
-                    software_version="0.1.0",
-                    start_date_time=start_date_time,  # TODO: Add actual dt
-                    end_date_time=dt.now(),  # TODO: Add actual dt
-                    input_location=input_fp,
-                    output_location=str(output_fp),
-                    code_url=(url),
-                    parameters=metadata,
-                )
-            ],
-        )
-    )
-    prev_processing.processing_pipeline.data_processes.append(processing.processing_pipeline.data_processes[0])
-    prev_processing.write_standard_file(output_directory=Path(output_fp).parent)
 
 def get_and_plot_epilepsy_probability(
     cropped_video, frame_rate, signal_threshold=10.0, min_width=0.1, max_width=0.3
@@ -835,10 +787,3 @@ if __name__ == "__main__":  # pragma: nocover
     with open(os.path.join(output_dir, base_file + "_metrics.json"), "w") as f:
         json.dump(metrics, f, indent=4)
     
-    write_output_metadata(
-        metrics,
-        str(h5_file),
-        os.path.join(output_dir, base_file + "_metrics.json"),
-        "https://github.com/AllenNeuralDynamics/aind-ophys-movie-qc",
-        start_time,
-    )
