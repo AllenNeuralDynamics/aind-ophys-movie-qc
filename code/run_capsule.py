e9fcbb83b3d4254492e2810ece3afa1951a453cb
jkim0731 <jinho.kim@alleninstitute.org>
2025-06-18 18:07:40 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1625,10 +1625,7 @@ if __name__ == "__main__":  # pragma: nocover
     )
     
     # z-drift metrics
-    # session_json_path = next(h5_file.parent.parent.parent.glob('session.json'))
     session_json_path = next(Path(args.input_dir).rglob("session.json"))
-    print(session_json_path)
-    # zstack_filepath = next(h5_file.parent.parent.glob('*_z_stack_local.h5'))
     zstack_filepath = next(Path(args.input_dir).rglob(f'{unique_id}_z_stack_local.h5'))
     local_zstack = LocalZStack(zstack_filepath=zstack_filepath,
                                physio_filepath=h5_file,
