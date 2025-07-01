b6bf5c6cdc66ef3b38656a0151a63a48cf276c56
Jinho Kim <jkimkj@gmail.com>
2025-07-01 14:08:04 -0700

    add a comma

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -286,7 +286,7 @@ def write_qc_evaluation(output_dir: Path, unique_id: str, metrics: dict) -> None
         name=f"{unique_id} Z-drift Analysis",
         description="Z-drift analysis metrics",
         value=zdrift_metrics_dict,
-        reference=str(f"{unique_id}/movie_qc/{unique_id}_registered_zdrift.png")
+        reference=str(f"{unique_id}/movie_qc/{unique_id}_registered_zdrift.png"),
         status_history=[QCStatus(evaluator="Automated", timestamp=dt.now(), status=zdrift_status)],
     )
 
