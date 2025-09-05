aed46a97615473d53095e0254d31764e7b7b2ae3
arielleleon <54869022+arielleleon@users.noreply.github.com>
2025-09-05 12:46:49 -0700

    fix: save reg zstack in movie qc (#27)
    * fix: save reg zstack in movie qc
    
    * fix: remove list all in data directory
    
    * fix: remove print
    
    * fix: remove raw z stack file path creation

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1550,10 +1550,8 @@ if __name__ == "__main__":  # pragma: nocover
         save_figure_to_storage(fig, output_dir, base_file, f"zdrift", dpi=300)
 
         # save z-stack files (both raw and registered)
-        zstack_save_filepath = Path(args.output_dir) / unique_id / zstack_filepath.name
-        shutil.copy(str(zstack_filepath), str(zstack_save_filepath))
         zstack_reg_save_filepath = (
-            Path(args.output_dir) / f"{unique_id}/{zstack_filepath.stem}_reg.h5"
+            Path(args.output_dir) / f"{unique_id}/movie_qc/{zstack_filepath.stem}_reg.h5"
         )
         with h5py.File(zstack_reg_save_filepath, "w") as h:
             h.create_dataset("data", data=local_zstack.zstack)
