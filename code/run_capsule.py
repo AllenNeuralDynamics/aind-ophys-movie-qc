dfb84ae21a3dfbe976a163d3a074039f7f7a45fd
seanmcculloch <sean.mcculloch@alleninstitute.org>
2025-12-05 05:02:34 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -338,18 +338,6 @@ def write_qc_evaluation(
             ),
         }
 
-        local_z_stack_slider = QCMetric(
-            name=f"{unique_id} Local Stack Slider",
-            description=f"Interactive slider for {unique_id} local_z_stack.h5",
-            value="slider",
-            reference=zdrift["zstack_reg_filepath"],
-            status_history=[
-                QCStatus(
-                    evaluator="Automated", timestamp=dt.now(), status=Status.PASS
-                )
-            ]
-        )
-
         zdrift_metrics = QCMetric(
             name=f"{unique_id} Z-drift Analysis",
             description="Z-drift analysis metrics",
@@ -368,7 +356,6 @@ def write_qc_evaluation(
             name=f"Op. QC: Z-drift",
             description=f"Analysis of z-drift in the recording, with threshold {zdrift_qc_threshold} um",
             allow_failed_metrics=False,
-            # metrics=[local_z_stack_slider, zdrift_metrics],
             metrics=[zdrift_metrics],
             tags=['Operational QC']
         )
